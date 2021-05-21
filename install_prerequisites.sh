b4d94dc7f907a8768c73a2680c12cd15f7f29cf3
Isaac Harris-Holt <isaac@harris-holt.com>
2021-05-21 20:55:33 +0100

    Fixed issues with chromedriver permissions on Mac and Linux

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,3 +1,2 @@
-apt-get install python3-pip || sudo apt-get install python3-pip
+apt-get install python3-pip || sudo apt-get install python3-pip || brew install python3-pip
 python3 -m pip install -r requirements.txt || sudo python3 -m pip install -r requirements.txt || python -m pip install -r requirements.txt || sudo python -m pip install -r requirements.txt
-apt-get install python3-tk || sudo apt-get install python3-tk
