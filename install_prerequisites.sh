dfce73b60cc4ae338a4ca61abd19d88172d09cbe
Isaac Harris-Holt <isaac@harris-holt.com>
2021-05-22 20:05:40 +0100

    Fixing runtime issues for MacOS

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,6 +1,5 @@
 apt-get install python3-pip || sudo apt-get install python3-pip || apt install python3-pip || sudo apt install python3-pip || brew install python3-pip
 python3 -m pip install -r requirements.txt || sudo python3 -m pip install -r requirements.txt || python -m pip install -r requirements.txt || sudo python -m pip install -r requirements.txt
-apt-get install chromium-chromedriver || sudo apt-get install chromium-chromedriver || apt install chromium-chromedriver || sudo apt install chromium-chromedriver || brew cask install chromium-chromedriver
-cd ./files || exit
-xattr -d com.apple.quarantine chromedriver_mac || echo
-spctl --add --label 'Approved' chromedriver_mac || echo
+apt-get install chromium-chromedriver || sudo apt-get install chromium-chromedriver || apt install chromium-chromedriver || sudo apt install chromium-chromedriver || brew install chromium-chromedriver
+xattr -d com.apple.quarantine ./files/chromedriver_mac || echo
+spctl --add --label 'Approved' ./files/chromedriver_mac || echo
